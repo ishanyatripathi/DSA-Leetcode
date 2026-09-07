0bfeab14f5e6cdfa3e4e397fa07dce8ce99656cd
Ishanya Tripathi <ishanya.tripathi15@gmail.com>
2026-09-07 19:50:05 +0530

    Day 01: Two Sum

---
+++ Arrays/0001_Two_Sum.py
@@ -0,0 +1,16 @@
+# LeetCode 1 - Two Sum
+# Difficulty: Easy
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in seen:
+                return [seen[complement], i]
+
+            seen[num] = i
