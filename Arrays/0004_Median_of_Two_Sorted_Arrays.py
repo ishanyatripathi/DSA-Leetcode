93c7abf0d51d94de8cfa8f81707dc55f9d65d597
Ishanya Tripathi <ishanya.tripathi15@gmail.com>
2026-09-08 21:17:04 +0530

    Day 02: Median of Two Sorted Arrays

---
+++ Arrays/0004_Median_of_Two_Sorted_Arrays.py
@@ -0,0 +1,36 @@
+# LeetCode 4 - Median of Two Sorted Arrays
+# Difficulty: Hard
+# Time Complexity: O(log(min(m, n)))
+# Space Complexity: O(1)
+
+class Solution(object):
+
+    def findMedianSortedArrays(self, nums1, nums2):
+
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        m, n = len(nums1), len(nums2)
+        left, right = 0, m
+
+        while left <= right:
+            partition1 = (left + right) // 2
+            partition2 = (m + n + 1) // 2 - partition1
+
+            left1 = float('-inf') if partition1 == 0 else nums1[partition1 - 1]
+            right1 = float('inf') if partition1 == m else nums1[partition1]
+
+            left2 = float('-inf') if partition2 == 0 else nums2[partition2 - 1]
+            right2 = float('inf') if partition2 == n else nums2[partition2]
+
+            if left1 <= right2 and left2 <= right1:
+                if (m + n) % 2 == 1:
+                    return float(max(left1, left2))
+
+                return (max(left1, left2) + min(right1, right2)) / 2.0
+
+            elif left1 > right2:
+                right = partition1 - 1
+
+            else:
+                left = partition1 + 1
