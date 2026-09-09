aaf35457e625deb51b6aace1f0bf2b9838c155c1
Ishanya Tripathi <ishanya.tripathi15@gmail.com>
2026-09-09 23:38:56 +0530

    Day 03: Container With Most Water

---
+++ Arrays/0011_Container_With_Most_Water.py
@@ -0,0 +1,23 @@
+# LeetCode 11 - Container With Most Water
+# Difficulty: Medium
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+class Solution(object):
+    def maxArea(self, height):
+        left = 0
+        right = len(height) - 1
+        max_water = 0
+
+        while left < right:
+            width = right - left
+            current_height = min(height[left], height[right])
+
+            max_water = max(max_water, width * current_height)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_water
